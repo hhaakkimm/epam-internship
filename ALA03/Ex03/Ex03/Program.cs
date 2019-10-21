1a1e0fc8a9d9198278165d3fd069e1ca4018e215
hhaakkimm <hhaakkimm@gmail.com>
2019-10-21 08:17:12 +0600

    polyfuck

---
+++ ALA03/Ex03/Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex03
+{
+    class WeirdStuff
+    {
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Polynomial[] polCoefficents = new Polynomial[5];
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    internal class Polynomial
+    {
+    }
+}
