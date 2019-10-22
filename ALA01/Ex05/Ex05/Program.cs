55813ee3a6069c4faee1f81e0d7610353901f3f9
hhaakkimm <hhaakkimm@gmail.com>
2019-10-22 09:04:37 +0600

    added tasks from ala01

---
+++ ALA01/Ex05/Ex05/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Ex05
+{
+    class Program
+    {
+        static int StringToInt(string s)
+        {
+            int ans;
+
+            ans = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                ans = ans * 10 + (s[i] - '0');
+            }
+            return (ans);
+        }
+
+        static int getNextPermutation(string s)
+        {
+            int i = s.Length - 1;
+
+            char[] array = s.ToCharArray();
+            while (i > 0 && array[i - 1] >= array[i])
+                i--;
+            if (i <= 0)
+                return StringToInt(s);
+
+            int j = s.Length - 1;
+            while (array[j] <= array[i - 1])
+                j--;
+            char temp = array[i - 1];
+            array[i - 1] = array[j];
+            array[j] = temp;
+
+            j = s.Length - 1;
+            while (i < j)
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+            s = new String(array);
+            return StringToInt(s);
+        }
+
+        static int FindNextBiggerNumber(int num)
+        {
+            string  s;
+            int     ans;
+
+            s = num.ToString();
+            ans = getNextPermutation(s);
+            return (ans);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNextBiggerNumber(17892));
+        }
+    }
+}
---
+++ ALA01/Ex06/Ex06/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Ex06
+{
+    class Program
+    {
+        static bool check(int x, int digit)
+        {
+            while (x > 0)
+            {
+                if (x % 10 == digit)
+                    return (true);
+                x /= 10;
+            }
+            return (false);
+        }
+        static ArrayList FilterDigit(int[] Array, int digit)
+        {
+            ArrayList ans = new ArrayList();
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (check(Array[i], digit))
+                    ans.Add(Array[i]);
+            }
+            return (ans);
+        }
+        static void Main(string[] args)
+        {
+            ArrayList nums = new ArrayList();
+            int[] Array = {7,1,2,3,4,5,6,7,68,69,70, 15,17};
+            nums = FilterDigit(Array, 7);
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+        }
+    }
+}
